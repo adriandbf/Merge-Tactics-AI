5adc913d26e6525acf51730619ae2aaeb87aa45e
adrian fudge <adrianfudge0@gmail.com>
2025-09-19 16:26:51 -0400

    Tested troop detection

--- detectiontest.py
+++ detectiontest.py
@@ -2,16 +2,30 @@ from inference_sdk import InferenceHTTPClient
 
 client = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
-    api_key="YOUR_API_KEY"
+    api_key="YOUR API KEY"
 )
 
 result = client.run_workflow(
     workspace_name="adriandbf",
-    workflow_id="custom-workflow",
+    workflow_id="carddetection",
     images={
         "image": "knight.png"
     },
     use_cache=True # cache workflow definition for 15 minutes
 )
 
-print(result)
+# print(result)
+
+result2 = client.run_workflow(
+    workspace_name="adriandbf",
+    workflow_id="troopdetection",
+    images={
+        "image": "troopstest.png"
+    },
+    use_cache=True # cache workflow definition for 15 minutes
+)
+
+preds = result2[0]["predictions"]["predictions"]
+
+for p in preds:
+    print(p['class'])
