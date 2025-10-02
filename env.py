44907b818903981b32364cf98a60fd9ff137b553
Vera Schmitt <vera.schmitt@student.uibk.ac.at>
2025-10-02 15:48:54 +0200

    env function outlines needed for training added

---
+++ env.py
@@ -0,0 +1,9 @@
+
+class ClashRoyaleEnv:
+
+    # this method should start a new game and reset the parameters
+    def reset(self):
+
+    # a function that performs a given action in teh actual game and returns the next_state, reward, done-flag
+    def step(self, action_index):
+        return ...
